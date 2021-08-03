a5b28627f119c25f8b632f3fa577cf29450d1223
RichardKim <hiddeneventor@gmail.com>
2021-08-03 23:57:31 +0900

    회원가입화면 컴포넌트 분리 호출

--- lib/pages/user/join_page.dart
+++ lib/pages/user/join_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/pages/components/custom_elevated_button.dart';
+import 'package:flutter_blog/pages/components/custom_text_form_field.dart';
 
 class JoinPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: [],
+        children: [
+          SizedBox(height: 200),
+          CustomTextFormField(hint: "Enter UserName"),
+          CustomTextFormField(hint: "Enter Password"),
+          CustomTextFormField(hint: "Enter Email"),
+          CustomElevatedButton(text: "회원가입",)
+        ],
       ),
     );
   }
 }
+
