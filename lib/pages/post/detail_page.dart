b39cb875b8f6ba6602fdb720c36d380348093a1d
RichardKim <hiddeneventor@gmail.com>
2021-08-10 00:26:17 +0900

    [삭제, 수정 페이지] - 글 내용이 많을 경우 스크롤 작업 추가 완료 - Get.off 사용 방법

--- lib/pages/post/detail_page.dart
+++ lib/pages/post/detail_page.dart
@@ -1,8 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/pages/post/home_page.dart';
+import 'package:flutter_blog/pages/post/update_page.dart';
 import 'package:get/get.dart';
 
 class DetailPage extends StatelessWidget {
-
   final int id;
 
   const DetailPage(this.id);
@@ -12,8 +15,40 @@ class DetailPage extends StatelessWidget {
     // String data = Get.arguments; => Getx를 통해 테이터를 받을 떄 사용
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text("Detatil Page $id"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "글 제목!!",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+            ),
+            Divider(),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.off(HomePage()); /* 뒤로가기 말고 새로 객체를 만들어서 이동해야한다. 나중에 상태관리로 뺴야하는 작업 */
+                  },
+                  child: Text("삭제"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.to(UpdatePage());
+                  },
+                  child: Text("수정"),
+                ),
+              ],
+            ),
+            Expanded(
+              /* 글내용이 엄청 길 경우를 대비해야한다. (스크롤 용도)*/
+              child: SingleChildScrollView(
+                child: Text("글 내용 !!! " * 500),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
