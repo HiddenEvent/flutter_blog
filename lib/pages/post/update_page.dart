3149e49cc501ea2f1608c4a4d23bf13812d8df5b
RichardKim <hiddeneventor@gmail.com>
2021-08-11 23:49:15 +0900

    [글수정하기 페이지] - 글상세 데이터 추가 및 글수정 데이터 전송

--- lib/pages/post/update_page.dart
+++ lib/pages/post/update_page.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/pages/components/custom_elevated_button.dart';
+import 'package:flutter_blog/pages/components/custom_text_form_field.dart';
+import 'package:flutter_blog/pages/components/custom_textarea.dart';
+import 'package:flutter_blog/pages/post/detail_page.dart';
+import 'package:flutter_blog/util/validator_util.dart';
+import 'package:get/get.dart';
+
+import 'home_page.dart';
 
 class UpdatePage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text("글수정화면"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                CustomTextFormField(
+                    hint: "Title", funValidator: validateTitle(),
+                  value: "제목~1",
+                ),
+                CustomTextArea(
+                    hint: "Content", funValidator: validateContent(),
+                  value: "내용~1"*20,
+                ),
+                CustomElevatedButton(
+                    text: "글수정하기",
+                    funPageRoute: () {
+                      if (_formKey.currentState!.validate()) {
+                        Get.off(DetailPage(1));
+                      }
+                    }),
+              ],
+            )),
       ),
     );
   }
