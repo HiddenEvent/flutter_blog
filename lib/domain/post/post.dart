d31b39b649bb31317db6d6fda90129cf90aebbdd
RichardKim <hiddeneventor@gmail.com>
2021-08-16 23:56:58 +0900

    게시판 리스트 데이터 json 파라미터 수정

--- lib/domain/post/post.dart
+++ lib/domain/post/post.dart
@@ -1,22 +1,15 @@
 import 'package:flutter_blog/domain/user/user.dart';
-import 'package:intl/intl.dart';
 
 class Post {
   final int? id;
   final String? title;
   final String? content;
-  final User? user;
-  final DateTime? created;
-  final DateTime? updated;
 
-  Post({this.id, this.title, this.content, this.user, this.created, this.updated});
+  Post({this.id, this.title, this.content});
 
   // 통신을 위한 Dart 오브젝트
   Post.fromJson(Map<String, dynamic> json)
       : id = json["id"],
-        title = json["username"],
-        content = json["email"],
-        user = User.fromJson(json["user"]),
-        created = DateFormat("yyyy-mm-dd").parse(json["created"]),
-        updated = DateFormat("yyyy-mm-dd").parse(json["updated"]);
+        title = json["title"],
+        content = json["content"];
 }
