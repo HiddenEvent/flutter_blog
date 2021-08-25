4508a41176ee575c0eb270ec6e9c3f20ea3a66f3
RichardKim <hiddeneventor@gmail.com>
2021-08-25 22:38:55 +0900

    1. 사용자 정보 출력 2. navigator 글로벌 키로 drawer 컨트롤 하기 완료

--- lib/view/pages/post/home_page.dart
+++ lib/view/pages/post/home_page.dart
@@ -10,22 +10,39 @@ import 'package:get/get.dart';
 import 'detail_page.dart';
 
 class HomePage extends StatelessWidget {
-  var refreshKey = GlobalKey<RefreshIndicatorState>(); /* 리프레쉬 하기위해 생성 */
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
+
+  /* 리프레쉬 하기위해 생성 */
+  var scaffoldKey = GlobalKey<ScaffoldState>();
 
   // put 없으면 만들고,  있으면 찾는다 (싱글턴으로 관리됨)
   final UserController userC = Get.find();
+
   // 객체 생성(create), onInit() 함수 실행 initialize)
   final PostController postC = Get.put(PostController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if(scaffoldKey.currentState!.isDrawerOpen){ /* 드로어가 열려있으면 */
+            scaffoldKey.currentState!.openEndDrawer(); //드로어를 닫겠다
+          } else {
+            scaffoldKey.currentState!.openDrawer(); // 드로어를 열겠다.
+          }
+        },
+        child: Icon(Icons.code),
+      ),
       drawer: _navigation(context),
       appBar: AppBar(
         title: Text("${userC.isLogin}"),
       ),
-      body: Obx( /* obx를 Db데이터로 리프레쉬 할수 없다. => getBuilder를 사용해서 DB값으로 리프레쉬 해야한다.*/
-        () => RefreshIndicator( /* 리프레쉬 하기위한 위젯*/
+      body: Obx(
+        /* obx를 Db데이터로 리프레쉬 할수 없다. => getBuilder를 사용해서 DB값으로 리프레쉬 해야한다.*/
+        () => RefreshIndicator(
+          /* 리프레쉬 하기위한 위젯*/
           key: refreshKey,
           onRefresh: () async {
             await postC.findAll(); /* 리프레쉬 함수... 화면을 아래로 끌어내리면 리프래쉬됨*/
@@ -77,7 +94,9 @@ class HomePage extends StatelessWidget {
               Divider(),
               TextButton(
                 onPressed: () {
-                  Get.to(UserInfoPage());
+                  Navigator.pop(context); /* 화면을 이동하면서 Navigator를 닫는 작업 - 1방법*/
+                  // scaffoldKey.currentState!.openEndDrawer(); /* 화면을 이동하면서 Navigator를 닫는 작업 - 2방법*/
+                  Get.to(() => UserInfoPage());
                 },
                 child: Text(
                   "회원정보보기",
